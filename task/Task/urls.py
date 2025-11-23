9e19a1e4a3ca00569d75f5cb48e5312c8bc3ba81
Sonu Lodhi <sp95759201@gmail.com>
2025-11-23 20:28:20 +0530

    change the home path

--- task/Task/urls.py
+++ task/Task/urls.py
@@ -24,7 +24,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Home, ),
-    path('app/',include('app.urls') )
+    path('',include('app.urls') )
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
